4756ed332d8e3a125119e96b122d64daf8456f30
rootadmin12321 <rp.rohit0099@gmail.com>
2025-10-06 15:05:45 +0530

    new project add tomato food delivery app

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+import 'dotenv/config';
+
+// App config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// DB connection
+connectDB();
+
+// API endpoints
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static('uploads'));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+// Test route
+app.get("/", (req, res) => res.send("API Working"));
+
+// Start server
+app.listen(port, () => console.log(`🚀 Server started on http://localhost:${port}`));
